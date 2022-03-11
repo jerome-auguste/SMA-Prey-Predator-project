d1eb1bbce64a22ae58e47963fb59b6ed823e54a9
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2022-03-11 18:21:12 +0100

    Chasing mode

--- prey_predator/agents.py
+++ prey_predator/agents.py
@@ -1,4 +1,5 @@
 """Agents definition classes (Sheep, Wolf or GrassPatch)"""
+import operator
 from mesa import Agent
 from prey_predator.random_walk import RandomWalker
 
@@ -12,17 +13,18 @@ class Sheep(RandomWalker):
 
     energy = None
 
-    def __init__(self, unique_id, pos, model, moore, energy=None):
+    def __init__(self, unique_id, pos, model, moore, energy=None, chasing_mode=False):
         super().__init__(unique_id, pos, model, moore=moore)
         self.energy = energy
-        
+        self.chasing_mode = chasing_mode
+
     def try_reproduce(self):
         if self.random.random() <= self.model.sheep_reproduce:
             self.energy //= 2
             a = Sheep(self.model.next_id(), self.pos, self.model, self.moore, energy=self.energy)
             self.model.schedule.add(a)
             self.model.grid.place_agent(a, self.pos)
-            
+
     def try_eat(self):
         cell_agents = self.model.grid.get_cell_list_contents([self.pos])
         for agent in cell_agents:
@@ -30,24 +32,48 @@ class Sheep(RandomWalker):
                 self.energy += self.model.sheep_gain_from_food
                 agent.fully_grown = False
                 break
-    
+
     def try_die_from_energy(self):
         if self.energy <= 0:
             self.model.schedule.remove(self)
             self.model.grid.remove_agent(self)
-            
+
+    def runaway_move(self):
+        neighbor_cells = self.model.grid.get_neighborhood(self.pos,
+                                                          moore=self.moore,
+                                                          include_center=False)
+
+        neighbor_score = {neighbor_cell: 0 for neighbor_cell in neighbor_cells}
+        for neighbor_cell in neighbor_cells:
+            cell_agents = self.model.grid.get_cell_list_contents([neighbor_cell])
+            for agent in cell_agents:
+                if isinstance(agent, Wolf):
+                    neighbor_score[agent.pos] -= 2
+                    enemy_possible_moves = self.model.grid.get_neighborhood(
+                        agent.pos, moore=self.moore, include_center=False)
+                    for bad_position in (set(enemy_possible_moves) & set(neighbor_cells)):
+                        neighbor_score[bad_position] -= 1 
+                if isinstance(agent, GrassPatch) and agent.fully_grown:
+                    neighbor_score[agent.pos] += 1
+        move_score = list(neighbor_score.items())
+        self.random.shuffle(move_score)
+        next_move = max(move_score, key = lambda x: x[1])[0]
+        self.model.grid.move_agent(self, next_move)
 
     def step(self):
         """
         A model step. Move, then eat grass and reproduce.
         """
         # ... to be completed
-        self.random_move()
+        if self.chasing_mode:
+            self.runaway_move()
+        else:
+            self.random_move()
         self.energy -= 1
         self.try_reproduce()
         self.try_eat()
         self.try_die_from_energy()
-        
+
 
 
 class Wolf(RandomWalker):
@@ -57,40 +83,61 @@ class Wolf(RandomWalker):
 
     energy = None
 
-    def __init__(self, unique_id, pos, model, moore, energy=None):
+    def __init__(self, unique_id, pos, model, moore, energy=None, chasing_mode=False):
         super().__init__(unique_id, pos, model, moore=moore)
         self.energy = energy
-    
+        self.chasing_mode = chasing_mode
+
     def try_reproduce(self):
         if self.random.random() <= self.model.wolf_reproduce:
             self.energy //= 2
             a = Wolf(self.model.next_id(), self.pos, self.model, self.moore, energy=self.energy)
             self.model.schedule.add(a)
             self.model.grid.place_agent(a, self.pos)
-    
+
     def try_eat(self):
         cell_agents = self.model.grid.get_cell_list_contents([self.pos])
         for agent in cell_agents:
             if isinstance(agent, Sheep):
-                self.energy += self.model.wolf_gain_from_food
+                self.energy += int(self.model.wolf_gain_from_food * (
+                    min(1, agent.energy / self.model.initial_sheep_energy))) # Variable energy gain from eating sheep
                 self.model.schedule.remove(agent)
                 self.model.grid.remove_agent(agent)
                 break
-    
+
     def try_die_from_energy(self):
         if self.energy <= 0:
             self.model.schedule.remove(self)
             self.model.grid.remove_agent(self)
-        
+
+    def chasing_move(self):
+        neighbor_cells = self.model.grid.get_neighborhood(self.pos,
+                                                          moore=self.moore,
+                                                          include_center=False)
+
+        neighbor_score = {neighbor_cell: 0 for neighbor_cell in neighbor_cells}
+        for neighbor_cell in neighbor_cells:
+            cell_agents = self.model.grid.get_cell_list_contents([neighbor_cell])
+            for agent in cell_agents:
+                if isinstance(agent, Sheep):
+                    neighbor_score[agent.pos] += 1
+        move_score = list(neighbor_score.items())
+        self.random.shuffle(move_score)
+        next_move = max(move_score, key = lambda x: x[1])[0]
+        self.model.grid.move_agent(self, next_move)
+
 
     def step(self):
         # ... to be completed
-        self.random_move()
+        if self.chasing_mode:
+            self.chasing_move()
+        else:
+            self.random_move()
         self.energy -= 1
         self.try_reproduce()
         self.try_eat()
         self.try_die_from_energy()
-        
+
 
 
 class GrassPatch(Agent):
@@ -111,7 +158,7 @@ class GrassPatch(Agent):
         self.fully_grown = fully_grown
         self.countdown = countdown
         self.current_countdown = self.countdown
-    
+
     def grow(self):
         self.current_countdown -= 1
         if self.fully_grown or self.current_countdown == 0:
--- prey_predator/model.py
+++ prey_predator/model.py
@@ -54,7 +54,8 @@ class WolfSheep(Model):
                  sheep_gain_from_food=4,
                  grass_countdown=10,
                  initial_sheep_energy=10,
-                 initial_wolf_energy=10):
+                 initial_wolf_energy=10,
+                 chasing_mode=False):
         """
         Create a new Wolf-Sheep model with the given parameters.
 
@@ -84,6 +85,7 @@ class WolfSheep(Model):
         self.grass_countdown = grass_countdown
         self.initial_sheep_energy = initial_sheep_energy
         self.initial_wolf_energy = initial_wolf_energy
+        self.chasing_mode = chasing_mode
 
         self.schedule = RandomActivationByBreed(self)
         self.grid = MultiGrid(self.height, self.width, torus=True)
@@ -100,7 +102,7 @@ class WolfSheep(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             a = Sheep(self.next_id(), (x, y), self, moore=True,
-                      energy=self.initial_sheep_energy) 
+                      energy=self.initial_sheep_energy, chasing_mode=self.chasing_mode) 
             self.schedule.add(a)
             self.grid.place_agent(a, (x, y))
 
@@ -110,14 +112,14 @@ class WolfSheep(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             a = Wolf(self.next_id(), (x, y), self, moore=True,
-                     energy=self.initial_wolf_energy)
+                     energy=self.initial_wolf_energy, chasing_mode=self.chasing_mode)
             self.schedule.add(a)
             self.grid.place_agent(a, (x, y))
 
         # Create grass patches
         # ... to be completed
-        for x in range(width):
-            for y in range(height):
+        for x in range(self.grid.width):
+            for y in range(self.grid.height):
                 a = GrassPatch(self.next_id(), (x, y),
                                self,
                                fully_grown=True,
@@ -126,12 +128,13 @@ class WolfSheep(Model):
                 self.grid.place_agent(a, (x, y))
 
     def step(self):
-        self.schedule.step()
+        # ... to be completed
+        self.schedule.step()          
 
         # Collect data
         self.datacollector.collect(self)
 
-        # ... to be completed
+
 
     def run_model(self, step_count=200):
 
